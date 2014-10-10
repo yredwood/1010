688fec2cc6431d566820354890736aeb24db0116
yredwood <yredwood@naver.com>
2014-10-10 15:54:29 +0900

    1010

--- 1010.py
+++ 1010.py
@@ -4,23 +4,33 @@ Target = []
 
 Lower = 0
 Upper = 100
-Count = 15
+Count = 100
 
 for i in xrange(Count):
 	Target.append(random.randint(Lower, Upper));
 
 print Target
 
-def bublebuble(target):
-	length = len(Target)
-	for i in range(length-1):
-		for j in range(i, length-1):
-			if target[j] > target[j+1]:
-				temp = target[j]
-				target[j] = target[j+1]
-				target[j+1] = temp
+def bubblesort(a) :
+	count = 1
+	for i in range(0,len(a)):
+		for j in range(i+1, len(a)):
 
-	return target
+			if(count % 10 == 1 and count % 100 != 11) :
+				print "....",count,"st comparison...."
+			elif(count % 10 == 2 and count % 100 != 12) :
+				print "....",count,"nd comparison...."
+			else:
+				print "....",count,"th comparison...."
 
+			print a[i],"(",i,") and ",a[j],"(",j,") comparison"
 
-print bublebuble(Target)
+			if(a[i] > a[j]) :
+				temp = a[i]
+				a[i] = a[j]
+				a[j] = temp
+			count = count +1
+
+	return a
+
+print bubblesort(Target)
